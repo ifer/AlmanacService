74c0ce1e158e62b0b05bab317017bd39fd232701
Ioannis Fertakis <ifertakis@gmail.com>
2020-08-04 12:38:21 +0300

    fixed and mobile holidays data OK

--- tests.js
+++ tests.js
@@ -1,12 +1,48 @@
-const getDayInfo = require('./dayinfo').getDayInfo;
-const moonPhases = require('./MoonPhases');
-var moment = require('moment');
+// const getDayInfo = require('./dayinfo').getDayInfo;
 
-// moonPhases.populateMoonPhases();
-//
-// let testDate = moment('04/08/2020', 'DD/MM/YYYY');
-// let mp = moonPhases.getMoonPhaseEvent(testDate);
-//
-// console.log(mp);
+// testMobileHolidays();
+testFixedHolidays();
 
-console.log(getDayInfo('01/10/2020'));
+function testMobileHolidays() {
+    mobileHolMap = require('./holidays').mobileHolMap;
+
+    console.log('get EASTER holiday: ' + mobileHolMap.get(0));
+    console.log('get CLEAN MONDAY holiday: ' + mobileHolMap.get(-48));
+    console.log('get mobile holidays number: ' + mobileHolMap.size);
+    console.log('check existence of key 50: ' + mobileHolMap.has(50));
+
+    // iterate
+    mobileHolMap.forEach((value, key) => {
+        console.log(`key:${key} value: ${value} `);
+    });
+}
+
+function testFixedHolidays() {
+    fixedHolMap = require('./holidays').fixedHolMap;
+
+    console.log('get St.JOHN holiday: ' + fixedHolMap.get('0701'));
+    console.log('get St NIKOLAS holiday: ' + fixedHolMap.get('0612'));
+    console.log('get fixed holidays number: ' + fixedHolMap.size);
+    console.log('check existence of key 50: ' + fixedHolMap.has('0601'));
+
+    // iterate
+    let n = 0;
+    let max = 10;
+    for (let item of fixedHolMap) {
+        console.log(`key:${item[0]} value: ${item[1]}`);
+        n++;
+        if (n == 10) break;
+    }
+}
+
+function testMoonPhases() {
+    var moment = require('moment');
+    const moonPhases = require('./MoonPhases');
+
+    let testDate = moment('04/08/2020', 'DD/MM/YYYY');
+    let mp = moonPhases.getMoonPhaseEvent(testDate);
+
+    console.log(mp);
+
+    console.log(getDayInfo('01/10/2020'));
+}
